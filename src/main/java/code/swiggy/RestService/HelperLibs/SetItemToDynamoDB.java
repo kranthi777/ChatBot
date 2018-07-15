98dd0d9f7010caa710b30cd61907e7dd1df8e398
Ashish Verma <ashish.verma@niki.ai>
2018-07-15 07:04:17 +0530

    changes

---
+++ src/main/java/code/swiggy/RestService/HelperLibs/SetItemToDynamoDB.java
@@ -0,0 +1,63 @@
+
+package code.swiggy.RestService.HelperLibs;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.regions.Regions;
+/**
+ * @author Ashish Verma
+ *
+ */
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+
+import java.util.Iterator;
+
+import org.json.JSONObject;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DeleteItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+
+public class SetItemToDynamoDB {
+	AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+			.withRegion(Regions.US_EAST_2)
+			.build(); 
+	DynamoDB dynamoDB = new DynamoDB(client);
+	
+	public void setItemByHash(String itemKey, JSONObject thingsToPut, String tableName)
+	{
+	Table table = dynamoDB.getTable(tableName);
+	Item item = new Item();
+	Iterator<?> keys = thingsToPut.keys();
+
+	while( keys.hasNext())
+	{
+	    String key = (String)keys.next();
+	    try 
+	    {
+	      String value = thingsToPut.getString("key");
+	      if(value.equalsIgnoreCase(itemKey))
+	      {
+	      item.withPrimaryKey(key,value);
+	      }
+	      else
+	      {
+	    	item.withString(key, value);
+	       }
+	    }
+	    catch(Exception e)
+	      {
+	    	
+	      }
+     }
+	  table.putItem(item);
+	}
+}
+
---
+++ src/main/java/code/swiggy/RestService/RequestReceiver/OrderManager.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package code.swiggy.RestService.RequestReceiver;
+
+import org.json.JSONObject;
+
+import code.swiggy.RestService.HelperLibs.GetItemFromDynamoDb;
+
+/**
+ * @author Ashish Verma
+ *
+ */
+public class OrderManager {
+	
+	public  String provideResponse(String sessionId,String msg)
+	{
+		String response;
+		String orderStatus="initial";
+		GetItemFromDynamoDb item= new GetItemFromDynamoDb();
+		try
+		{
+		JSONObject order= new JSONObject(item.getItemByHash(sessionId,"Session"));
+		orderStatus=order.getString("orderStatus");
+		}
+		catch(Exception e)
+		{
+			orderStatus="initial";
+		}
+		switch(orderStatus)
+		{
+		case "initial":
+			response = doIntialOrderProcessing(sessionId,msg);
+			break;
+		case "order_construction":
+			response = doOrderConstructionProcessing(sessionId,msg);
+         	break;
+		case "order_completion":
+			response = doOrderCompletion(sessionId,msg);
+			 break;
+		case "payment_processing":
+			response = doPayment(sessionId,msg);
+			 break;
+		case "order_confirmation":
+			response = confirmOrder(sessionId,msg);
+		     break;
+		}
+		return null;
+	}
+	public String doIntialOrderProcessing(String sessionId,String msg)
+	{
+		return "Hi Human!";
+	}
+    public String doOrderConstructionProcessing(String sessionId,String msg)
+    {
+    	return "Hi Human!";
+    }
+    public String doOrderCompletion(String sessionId,String msg)
+    {
+    	return "Hi Human!";
+    }
+    public String doPayment(String sessionId,String msg)
+    {
+    	return "Hi Human!";
+    }
+    public String confirmOrder(String sessionId,String msg)
+    {
+    	return "Hi Human!";
+    }
+}
